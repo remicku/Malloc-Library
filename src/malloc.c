23362f2460d64379626ecf88d2d9e1131d599b97
remi.brenaut <remi.brenaut@epita.fr>
2025-02-05 15:10:00 +0100

    feat: malloc library

---
+++ src/malloc.c
@@ -0,0 +1,23 @@
+#include <stddef.h>
+
+#include "my_malloc.h"
+
+__attribute__((visibility("default"))) void *malloc(size_t size)
+{
+    return my_malloc(size);
+}
+
+__attribute__((visibility("default"))) void free(void *ptr)
+{
+    my_free(ptr);
+}
+
+__attribute__((visibility("default"))) void *realloc(void *ptr, size_t size)
+{
+    return my_realloc(ptr, size);
+}
+
+__attribute__((visibility("default"))) void *calloc(size_t nmemb, size_t size)
+{
+    return my_calloc(nmemb, size);
+}
